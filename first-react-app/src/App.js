de3127259aafa01bbcf228084ec85eebd9a4e3b2
VedantDalal21 <dalalvedant2110@gmail.com>
2024-08-05 13:59:22 +0000

    Day-02

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import ClassComponent from './components/ClassComponents';
 import FunctionalComponents from './components/FunctionalComponents';
+import {useState} from "react";
 function App() {
+  const author = "Vedant";
+
+  const [name, setName] = useState("Jagannath");
   return (
     <div className="App">
       
@@ -9,7 +13,9 @@ function App() {
       <ClassComponent></ClassComponent>
       <br/>
       <p>Functional Components</p>
-      <FunctionalComponents></FunctionalComponents>
+      <FunctionalComponents name = {name} age= {54} author = {author} setName = {setName}/> 
+      {/* <FunctionalComponents name = 'Reaction' age= {20} author = {author}/> */}
+      {/* <FunctionalComponents></FunctionalComponents> */}
     </div>
   );
 }
--- first-react-app/src/components/FunctionalComponents.jsx
+++ first-react-app/src/components/FunctionalComponents.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState}  from "react";
 
-const FunctionalComponents = () =>{
+const FunctionalComponents = ({name , age, author, setName}) =>{
+    const [count, setCount] = useState(0);
+    const [changeName, setChangeName] = useState("");
+    const reduceCount = ()=>{
+        setCount(count-1)
+    }
     return(
         <div>
             <p>This is Functoinsl Components</p>
+            <button onClick = {() =>setCount(count+1)}>Click here to increment</button>
+            <button onClick = {reduceCount}>Click here to decrement</button>
+            <h2>{count}</h2>
+            <h4>My father is: {name} it is of {age} years old and my name  is {author}</h4>
+            
+            {/* <input onChange = {(e)=> console.log(e.target.value)}/> */}
+
+            <input onChange = {(e)=> setChangeName(e.target.value)}/>
+            <button onClick = {() => setName(changeName)}>Change name to..</button>
         </div>
     )
 }
